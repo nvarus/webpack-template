ba2847738f0c10613ce610d36a1861730d2e32d0
Алексей Варюхин <varyukhin.a@hotmail.com>
2023-06-16 16:41:42 +0400

    first

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,9 @@ const getUser = (age:number, name:string):object => {
 
 console.log(getUser(42, 'Alexey'))
 
-let number: number | string = 100
+let number: number = 100
 
 number = 10.2
-number = 'alx'
 
 console.log(number)
 
-const userName = document.querySelector('.userName')
-userName?.addEventListener('click', () => {
-	console.log('hello')
-})
